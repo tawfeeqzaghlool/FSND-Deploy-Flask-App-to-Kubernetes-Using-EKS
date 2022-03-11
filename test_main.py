250c3ff26571dd249d7af146b73f5ff214221e32
Tawfeeq Zaghlool <34836970+tawfeeqzaghlool@users.noreply.github.com>
2022-03-11 14:42:04 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,7 +4,6 @@ Tests for jwt flask app.
 import os
 import json
 import pytest
-
 import main
 
 SECRET = 'TestSecret'
@@ -19,7 +18,6 @@ def client():
     client = main.APP.test_client()
 
     yield client
-
 
 
 def test_health(client):
